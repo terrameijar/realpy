4d8c96cdbb6cdc3b82547cf4e45ce190c501053c
Vuyisile Ndlovu <terrameijar@gmail.com>
2015-12-24 17:23:45 +0200

    Added styles and sessions

--- flask-blog/blog.py
+++ flask-blog/blog.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, request, session,\
     flash, redirect, url_for, g
 import sqlite3
 import os
+from functools import wraps
 
 # configuration
 DATABASE = 'blog.db'
@@ -19,6 +20,17 @@ app.config.from_object(__name__)
 # function used for connecting to the database
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
 
 @app.route('/',methods=['GET', 'POST'])
 def login():
@@ -37,9 +49,33 @@ def logout():
     flash('You were logged out')
     return redirect(url_for('login'))
 
+#when a GET request is sent to view main.html the main function is wrapped by the login required def
 @app.route('/main')
+@login_required
 def main():
-    return render_template('main.html')
+    g.db = connect_db() # creates db connection
+    cur = g.db.execute('SELECT * from posts') # fetches data from posts
+    posts = [dict(title=row[0], post=row[1]) for row in 
+        cur.fetchall()] # assigns data retrieved from db to a dictionary --> posts
+    g.db.close()
+    return render_template('main.html', posts=posts) # passes that variable to main.html file
+
+@app.route('/add', methods=['POST'])
+@login_required
+def add():
+    title = request.form['title']
+    post = request.form['post']
+    if not title or not post:
+        flash("All fields are required. Please try again")
+        return redirect(url_for('main'))
+    else:
+        g.db = connect_db()
+        g.db.execute('INSERT INTO POSTS (title,post) values (?, ?)',
+            [title, post])
+        g.db.commit()
+        g.db.close()
+        flash('New entry was successfully posted!')
+        return redirect(url_for('main'))
 
 if __name__ == '__main__':
     app.run(debug=True)
