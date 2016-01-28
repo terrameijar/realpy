03646b78815a63dbbdc239af376f2bdf0b79df67
Vuyisile Ndlovu <terrameijar@gmail.com>
2016-01-28 07:44:23 +0200

    Added SQLAlchemy support

---
+++ flasktaskr/app/config.py
@@ -0,0 +1,18 @@
+# config.py 
+
+import os
+
+# grabs the folder where the script runs
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATABASE = 'flasktaskr.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+WTF_CSRF_ENABLED = True #prevents cross-site request forgery
+SECRET_KEY = 'my_precious'
+
+#defines the full path for the database 
+DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+# the database URI
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
---
+++ flasktaskr/app/db_create.py
@@ -0,0 +1,10 @@
+from views import db
+from models import Task
+from datetime import date
+
+# create the database and the db table
+db.create_all()
+
+db.session.add(Task("Finish this tutorial", date(2016,1,29),10,1))
+db.session.add(Task("Finish Real Python!",date(2016,1,30),10,1))
+db.session.commit()
